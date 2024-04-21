733315cbbf3d7267994324a88b7cb17e45b7200a
Daiki Kasahara <dforce1002@gmail.com>
2024-04-21 16:33:51 +0900

    [update]アプリケーションの起動処理を更新

--- MagicSettings/BackgroundProcesses/KeyBindingListener/App.xaml.cs
+++ MagicSettings/BackgroundProcesses/KeyBindingListener/App.xaml.cs
@@ -1,5 +1,25 @@
-﻿namespace KeyBindingListener;
+﻿using System.Windows;
+
+namespace KeyBindingListener;
 
 public partial class App : System.Windows.Application
 {
+    private Mutex? _mutex;
+    private Window? _window;
+
+    private void Application_Startup(object sender, System.Windows.StartupEventArgs e)
+    {
+        _mutex = new Mutex(false, "MagicSettings.KeyBindingListener");
+
+        if (!_mutex.WaitOne(0, false))
+        {
+            _mutex.Close();
+            _mutex = null;
+
+            this.Shutdown();
+        }
+
+        _window = new MainWindow();
+        _window.Show();
+    }
 }
