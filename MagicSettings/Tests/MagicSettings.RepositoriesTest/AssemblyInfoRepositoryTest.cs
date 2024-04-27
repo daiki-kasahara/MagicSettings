48f569652cdf27ac6a9a92c515e6b18f278ccd1d
Daiki Kasahara <dforce1002@gmail.com>
2024-04-27 22:06:49 +0900

    [add]RepositoryTestの追加

---
+++ MagicSettings/Tests/MagicSettings.RepositoriesTest/AssemblyInfoRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using MagicSettings.Repositories.Repositories;
+
+namespace MagicSettings.RepositoriesTest;
+
+public class AssemblyInfoRepositoryTest
+{
+    [Fact]
+    public async Task GetAsyncTest()
+    {
+        // Arrange
+
+        // Action
+        var repository = new AssemblyInfoRepository();
+        var result = await repository.GetAsync();
+
+        // Assert
+        Assert.NotEqual(string.Empty, result.AppName);
+        Assert.NotEqual(string.Empty, result.AppVersion);
+        Assert.NotEqual(string.Empty, result.Copyright);
+    }
+}
