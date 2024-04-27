5905a28fbf3f20ad2776923a68e1879c2549afaa
Daiki Kasahara <dforce1002@gmail.com>
2024-04-28 00:07:48 +0900

    [add]KeyBindingListenerのテストを追加

---
+++ MagicSettings/Tests/KeyBindingListenerTest/Factories/ActionFactoryTest.cs
@@ -0,0 +1,50 @@
+﻿using KeyBindingListener.Contracts;
+using KeyBindingListener.Factories;
+using MagicSettings.Domains;
+
+namespace KeyBindingListenerTest.Factories;
+
+public class ActionFactoryTest
+{
+    [Theory]
+    [InlineData(KeyboardActionType.StartProgram)]
+    [InlineData(KeyboardActionType.OpenUrl)]
+    [InlineData(KeyboardActionType.ScreenCapture)]
+    [InlineData(KeyboardActionType.SnippingTool)]
+    [InlineData(KeyboardActionType.MicrosoftStore)]
+    [InlineData(KeyboardActionType.Settings)]
+    [InlineData(KeyboardActionType.SettingsBluetoothDevice)]
+    [InlineData(KeyboardActionType.SettingsCamera)]
+    [InlineData(KeyboardActionType.SettingsMouse)]
+    [InlineData(KeyboardActionType.SettingsKeyboard)]
+    [InlineData(KeyboardActionType.SettingsNetwork)]
+    [InlineData(KeyboardActionType.SettingsWifi)]
+    [InlineData(KeyboardActionType.SettingsSystemInfo)]
+    [InlineData(KeyboardActionType.SettingsSystemAppVolume)]
+    [InlineData(KeyboardActionType.SettingsSystemBatterySaver)]
+    [InlineData(KeyboardActionType.SettingsSystemBatterySaverSettings)]
+    [InlineData(KeyboardActionType.SettingsSystemBatteryUse)]
+    [InlineData(KeyboardActionType.SettingsSystemDisplay)]
+    [InlineData(KeyboardActionType.SettingsSystemFocusAssist)]
+    [InlineData(KeyboardActionType.SettingsSystemNightMode)]
+    [InlineData(KeyboardActionType.SettingsSystemNotification)]
+    [InlineData(KeyboardActionType.SettingsSystemSound)]
+    [InlineData(KeyboardActionType.SettingsSystemSoundDevice)]
+    [InlineData(KeyboardActionType.SettingsSystemStorage)]
+    public void Create(KeyboardActionType actionType)
+    {
+        // Arrange
+
+        // Action
+        var factory = new ActionFactory();
+        IAction action = factory.Create(actionType, string.Empty);
+        var exception = Record.Exception(() =>
+        {
+            action = factory.Create(actionType, string.Empty);
+        });
+
+        // Assert
+        Assert.Null(exception);
+        Assert.NotNull(action);
+    }
+}
---
+++ MagicSettings/Tests/KeyBindingListenerTest/Services/KeyHookServiceTest.cs
@@ -0,0 +1,139 @@
+﻿using KeyBindingListener.Contracts;
+using KeyBindingListener.Events;
+using KeyBindingListener.Services;
+using MagicSettings.Domains;
+using Moq;
+
+namespace KeyBindingListenerTest.Services;
+
+public class KeyHookServiceTest
+{
+    public KeyHookServiceTest()
+    {
+        // Setup
+        var serviceStub = new Mock<IKeyboardActionService>();
+        var service = new KeyHookService(serviceStub.Object);
+
+        // キー押しフラグをリセットする
+        service.OnKeyUp(null, new KeyboardHookEventArgs() { Key = VKeys.LeftWindows });
+        service.OnKeyUp(null, new KeyboardHookEventArgs() { Key = VKeys.LeftMenu });
+    }
+
+    // 代表のキーでいくつかチェックする
+    [Theory]
+    [InlineData(VKeys.A, VKeys.LeftWindows, VKeys.LeftMenu)]
+    [InlineData(VKeys.B, VKeys.RightWindows, VKeys.LeftMenu)]
+    [InlineData(VKeys.Y, VKeys.RightWindows, VKeys.RightMenu)]
+    [InlineData(VKeys.Z, VKeys.LeftWindows, VKeys.RightMenu)]
+    public void OnKeyDownTest(VKeys key, VKeys win, VKeys alt)
+    {
+        // Arrange
+        var serviceStub = new Mock<IKeyboardActionService>();
+        serviceStub.Setup(x => x.ActionAsync(key));
+
+        // Act
+        var service = new KeyHookService(serviceStub.Object);
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = win });
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = alt });
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = key });
+
+        // Assert
+        serviceStub.Verify(x => x.ActionAsync(key), Times.Once);
+    }
+
+    // 代表のキーでいくつかチェックする
+    [Theory]
+    [InlineData(VKeys.A)]
+    [InlineData(VKeys.B)]
+    [InlineData(VKeys.Z)]
+    public void OnKeyDownTest_NotMatch(VKeys key)
+    {
+        // Arrange
+        var serviceStub = new Mock<IKeyboardActionService>();
+        serviceStub.Setup(x => x.ActionAsync(It.IsAny<VKeys>()));
+
+        // Act
+        var service = new KeyHookService(serviceStub.Object);
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = key });
+
+        // Assert
+        // 単押しの場合は処理が実行されないこと
+        serviceStub.Verify(x => x.ActionAsync(It.IsAny<VKeys>()), Times.Never);
+    }
+
+    [Theory]
+    [InlineData(VKeys.LeftWindows)]
+    [InlineData(VKeys.RightWindows)]
+    public void OnKeyDownTest_OnlyWinKey(VKeys key)
+    {
+        // Arrange
+        var serviceStub = new Mock<IKeyboardActionService>();
+        serviceStub.Setup(x => x.ActionAsync(It.IsAny<VKeys>()));
+
+        // Act
+        var service = new KeyHookService(serviceStub.Object);
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = key });
+
+        // Assert
+        // 単押しの場合は処理が実行されないこと
+        serviceStub.Verify(x => x.ActionAsync(It.IsAny<VKeys>()), Times.Never);
+    }
+
+    [Theory]
+    [InlineData(VKeys.LeftMenu)]
+    [InlineData(VKeys.RightMenu)]
+    public void OnKeyDownTest_OnlyAltKey(VKeys key)
+    {
+        // Arrange
+        var serviceStub = new Mock<IKeyboardActionService>();
+        serviceStub.Setup(x => x.ActionAsync(It.IsAny<VKeys>()));
+
+        // Act
+        var service = new KeyHookService(serviceStub.Object);
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = key });
+
+        // Assert
+        // 単押しの場合は処理が実行されないこと
+        serviceStub.Verify(x => x.ActionAsync(It.IsAny<VKeys>()), Times.Never);
+    }
+
+    [Theory]
+    [InlineData(VKeys.LeftMenu)]
+    [InlineData(VKeys.RightMenu)]
+    public void OnKeyUpTest_WithAlt(VKeys key)
+    {
+        // Arrange
+        var serviceStub = new Mock<IKeyboardActionService>();
+        serviceStub.Setup(x => x.ActionAsync(It.IsAny<VKeys>()));
+
+        // Act
+        var service = new KeyHookService(serviceStub.Object);
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = VKeys.RightWindows });
+        service.OnKeyUp(null, new KeyboardHookEventArgs() { Key = key });
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = VKeys.A });
+
+        // Assert
+        // KeyUpしたときは処理が実行されないこと
+        serviceStub.Verify(x => x.ActionAsync(It.IsAny<VKeys>()), Times.Never);
+    }
+
+    [Theory]
+    [InlineData(VKeys.LeftWindows)]
+    [InlineData(VKeys.RightWindows)]
+    public void OnKeyUpTest_WithWin(VKeys key)
+    {
+        // Arrange
+        var serviceStub = new Mock<IKeyboardActionService>();
+        serviceStub.Setup(x => x.ActionAsync(It.IsAny<VKeys>()));
+
+        // Act
+        var service = new KeyHookService(serviceStub.Object);
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = VKeys.RightMenu });
+        service.OnKeyUp(null, new KeyboardHookEventArgs() { Key = key });
+        service.OnKeyDown(null, new KeyboardHookEventArgs() { Key = VKeys.A });
+
+        // Assert
+        // KeyUpしたときは処理が実行されないこと
+        serviceStub.Verify(x => x.ActionAsync(It.IsAny<VKeys>()), Times.Never);
+    }
+}
